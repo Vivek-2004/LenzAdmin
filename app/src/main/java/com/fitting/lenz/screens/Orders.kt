3e467a2cdadcefe512e2089b5c5833aea49293f2
Vivek Ghosh <ghoshvivek004@gmail.com>
2024-12-16 02:26:18 +0530

    Added Scroll Bar

--- app/src/main/java/com/fitting/lenz/screens/Orders.kt
+++ app/src/main/java/com/fitting/lenz/screens/Orders.kt
@@ -1,6 +1,7 @@
 package com.fitting.lenz.screens
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -8,6 +9,9 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.fitting.lenz.LenzViewModel
@@ -20,13 +24,24 @@ fun Orders(
     lenzViewModel: LenzViewModel = viewModel()
 ) {
     val listState = rememberLazyListState()
+    val scrollBarWidth = 5.dp
 
     LazyColumn(
-        modifier = Modifier.fillMaxSize(),
         state = listState,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize().drawBehind {
+            val elementHeight = this.size.height / listState.layoutInfo.totalItemsCount
+            val offset = listState.layoutInfo.visibleItemsInfo.first().index * elementHeight
+            val scrollbarHeight = listState.layoutInfo.visibleItemsInfo.size * elementHeight
+
+            drawRect(
+                color = colorScheme.compColor.copy(alpha = 0.1f),
+                topLeft = Offset(this.size.width - scrollBarWidth.toPx(), offset),
+                size = Size(scrollBarWidth.toPx(), scrollbarHeight)
+            )
+        }.padding(end = scrollBarWidth)
     ) {
-        items(15) { index ->
+        items(25) { index ->
             OrderItem(
                 colorScheme = colorScheme,
                 lenzViewModel = lenzViewModel,
